ade115dc6c8f18479ecd183157aed9635358a32b
JJUN_1012 <No public email provided>
2025-01-15 23:55:39 +0900

    Changes made from Source Academy

---
+++ Chapter2/2-2-.js
@@ -0,0 +1,12 @@
+function make_segment(a, b){return pair(a, b);}
+function start_segment(x){return head(x);}
+function end_segment(x){return tail(x);}
+function make_point(x, y){return pair(x,y);}
+function x_point(p){return head(p);}
+function y_point(p){return tail(p);}
+
+function midpoint_segment(s){
+    const midpoint_x = (x_point(start_segment(s)) + x_point(end_segment(s))) / 2;
+    const midpoint_y = (y_point(start_segment(s)) + y_point(end_segment(s))) / 2;
+    return make_point(midpoint_x, midpoint_y);
+}
