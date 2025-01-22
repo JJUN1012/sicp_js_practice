257d8d6a51267631e0893ced1281db6be0501f80
Source Academy User <JJUN_1012>
2025-01-22 22:44:13 +0900

    Changes made from Source Academy

--- Chapter2/2-7.js
+++ Chapter2/2-7.js
@@ -1,4 +1,4 @@
 function make_interval(x, y){ return pair(x, y);};
 
-function upper_bound(interval){return head(interval);};
-function lower_bound(interval){return tail(interval);};
+function lower_bound(interval){return head(interval);};
+function upper_bound(interval){return tail(interval);};
