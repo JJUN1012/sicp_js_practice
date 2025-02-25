c1270bb75b569b009e1c5827646382f69b1ea055
Source Academy User <JJUN_1012>
2025-02-25 22:50:46 +0900

    Changes made from Source Academy

--- Chapter2/2-12.js
+++ Chapter2/2-12.js
@@ -4,12 +4,18 @@ function lower_bound(interval){return head(interval);}
 function upper_bound(interval){return tail(interval);}
 
 function display_interval(interval){
-    return "[" + stringify(lower_bound(interval)) + ", "
-        + stringify(upper_bound(interval)) + "]";
+    display("[" + stringify(lower_bound(interval)) + ", "
+        + stringify(upper_bound(interval)) + "]");
 }
 
 function make_center_width(c, w) {
     return make_interval(c - w, c + w);
+}
+function center(i) {
+    return (lower_bound(i) + upper_bound(i)) / 2;
+}
+function width(i) {
+    return (upper_bound(i) - lower_bound(i)) / 2;
 }
 
 function make_center_percent(center_value, percent_value){
@@ -17,4 +23,9 @@ function make_center_percent(center_value, percent_value){
     return make_center_width(center_value, width);
 }
 
+function percent(x){
+    return width(x) / center(x) * 100;
+}
+
 display_interval(make_center_percent(30, 10));
+percent(make_interval(27,33));
