950bbf6cfdb37687b7d520abd88c36d8e7f2d84b
Source Academy User <JJUN_1012>
2025-02-27 22:33:32 +0900

    Changes made from Source Academy

--- Chapter2/2-32.js
+++ Chapter2/2-32.js
@@ -2,7 +2,7 @@ function subsets(s){
     if(is_null(s)){
         return list(null);
     }else{
-        const rest = subsets(tail(s));
+        const rest = subsets(tail(s));//subsets except head(s)
         return append(rest, map(set => pair(head(s), set), rest));
     }
 }
