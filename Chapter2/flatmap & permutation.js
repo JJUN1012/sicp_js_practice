7d150fb451a8a321eae0823c33109fe7a945d001
JJUN_1012 <46262005+JJUN1012@users.noreply.github.com>
2025-03-01 15:24:34 +0900

    Update and rename flatmap & permutation to flatmap & permutation.js

--- Chapter2/flatmap & permutation.js
+++ Chapter2/flatmap & permutation.js
@@ -2,8 +2,18 @@ function flatmap(f, seq){
     return accumulate(append, null, map(f, seq));
 }
 
+function square1(x){
+    return list(x * x);
+}
+
+const s = list(1, 2, 3);
+
+display_list(flatmap(square1, s));
+
 function permutations(s){
     return is_null(s)
         ? list(null)
         : flatmap(x => map(p => pair(x, p), permutations(remove(x, s))), s);
 }
+
+display_list(permutations(s));
