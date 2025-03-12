cad6383041f7f9a708156c19b766c1898b6ac7af
JJUN_1012 <46262005+JJUN1012@users.noreply.github.com>
2025-03-12 23:34:26 +0900

    Create 2-44.js

---
+++ Chapter2/2-44.js
@@ -0,0 +1,13 @@
+import { heart, stack, beside, show } from 'rune';
+
+function up_split(painter, n) {
+    if (n === 0) {
+        return painter;
+    } else {
+        const smaller = up_split(painter, n - 1);
+        return stack(beside(smaller, smaller), painter);
+        //return below(painter, beside(smaller, smaller));
+    }
+}
+
+show(up_split(heart, 4));
