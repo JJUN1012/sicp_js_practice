0a27ed2d237318d46bfa02bb1b7594a2fba2c053
JJUN_1012 <46262005+JJUN1012@users.noreply.github.com>
2025-03-14 20:18:05 +0900

    Update 2-46.js

--- Chapter2/2-46.js
+++ Chapter2/2-46.js
@@ -1,4 +1,4 @@
-function make_vet(xcor, ycor){
+function make_vect(xcor, ycor){
     return pair(xcor, ycor);
 }
 function xcor_vect(vect){
@@ -8,11 +8,15 @@ function ycor_vect(vect){
     return tail(vect);
 }
 function add_vect(v1, v2){
-    return make_vet(xcor_vect(v1) + xcor_vect(v2), ycor_vect(v1) + ycor_vect(v2));
+    return make_vect(xcor_vect(v1) + xcor_vect(v2), ycor_vect(v1) + ycor_vect(v2));
 }
 function sub_vect(v1, v2){
-    return make_vet(xcor_vect(v1) - xcor_vect(v2), ycor_vect(v1) - ycor_vect(v2));
+    return make_vect(xcor_vect(v1) - xcor_vect(v2), ycor_vect(v1) - ycor_vect(v2));
 }
 function scale_vect(s,v){
-    return make_vet(s * xcor_vect(v1), s * ycor_vect(v2));
+    return make_vect(s * xcor_vect(v), s * ycor_vect(v));
 }
+
+const my_vector_1 = make_vect(1, 2);
+const my_vector_2 = make_vect(3, 4);
+add_vect(my_vector_1, my_vector_2);
